e53e545ec122bbaef1a3828532cb4a537ac4b7d0
mian2699 <mian.2687@gmail.com>
2024-04-02 15:59:14 -0500

    cambios nuevos version ultima

---
+++ src/components/clients/mockData.js
@@ -0,0 +1,77 @@
+export const data = [
+    {
+      id: 1,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/428163084_797085412457828_5377639062681488263_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=pX7-JLKvMw0AX9g5P6-&_nc_ht=scontent.faqp1-1.fna&oh=00_AfCe-E83PJfsSfDbEHxaMdKd8Efuh7SYSGP9fPlykUtztQ&oe=6607F373',
+      
+    },
+    {
+      id: 2,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t1.6435-9/134983068_3599820596777586_3343995457192964432_n.png?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=zAPkFlmqeI8AX9ajor1&_nc_ht=scontent.faqp1-1.fna&oh=00_AfAzk5eD6i_w5oVcf6cPc-rOlHjCZ97D2Ls7WrfgPNiUxA&oe=662A7BD0',
+    },
+    {
+      id: 3,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/345448863_187087580894356_6544889624355268972_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=zecqumn0PT4AX9zsH7G&_nc_ht=scontent.faqp1-1.fna&oh=00_AfDXdhRamIhwhlkgFAg5yQHpAGmM4OTw_cYTViIaa5v3og&oe=6608DA09', 
+    },
+
+    
+
+    {
+      id: 4,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/326720459_723659359131359_4600201004351949983_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ssqkUPEmdBYAX91M3gq&_nc_ht=scontent.faqp1-1.fna&oh=00_AfBApn1bvnx-2Rjq23yfsUCHS6yche5_y8DARUQcNHu04g&oe=660857A6', 
+    },
+
+
+
+    {
+      id: 5,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/323408268_604443878188302_698751628828057539_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=u2nXuG3HZWEAX-y4YhX&_nc_ht=scontent.faqp1-1.fna&oh=00_AfCpg_raiKxYq1CDQb0e6QnpxcKFqUBhnEh93RpK2TC0ew&oe=66084039',
+    },
+
+  
+    {
+      id: 6,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/363394331_822526879605207_5200053676179375849_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=UNU1fBwsyXwAX86fWEw&_nc_ht=scontent.faqp1-1.fna&oh=00_AfB0Ch5MRWIZRZKsux7Ax4j90ZDc8TbugeEN-8x7pZM08A&oe=6608736E',
+    },
+
+
+    {
+      id: 7,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/298605537_443443494498120_2107739261428681382_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4WMbZbyXpssAX-LqMxu&_nc_ht=scontent.faqp1-1.fna&oh=00_AfDlscwIhXiSH5zBuECNuv9B9ZJ86dwNDAX_RzXC3CTAew&oe=6607DE03', 
+    },
+    
+
+    {
+      id: 8,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/434064718_122131878602198524_3890575540369569654_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=rHRTuDk1C5oAX_EDlln&_nc_ht=scontent.faqp1-1.fna&oh=00_AfDTU0-B9Z7hcOW2KqVobyHjPvBwS_cnRKpQn6EVhEYadA&oe=66073FBB',
+    },
+
+    {
+      id: 9,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/285516370_104172795655426_2665546655600413811_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=9YVVGVNGikEAX8Cj4T7&_nc_ht=scontent.faqp1-1.fna&oh=00_AfBx63Vz5bEZHR3n0KY_T3QxunOrI5KTRp2IRLH9TR08Kw&oe=6607E9A1',
+    },
+
+    
+
+    {
+      id: 10,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/358609629_1181527332697512_2656359256452394342_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=wVJcgmQHliwAX-Ay8P8&_nc_ht=scontent.faqp1-1.fna&oh=00_AfBY-hldXUPs1tGt-UUXZbBeXzkBrvDZ-QMFifJp3l0csg&oe=660884EC',
+    },
+
+    
+
+    {
+      id: 11,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t31.18172-8/13173081_503892959794850_7563164569273383841_o.png?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=GihzkziaBjUAX9KyYy4&_nc_ht=scontent.faqp1-1.fna&oh=00_AfBh1cIb0ltbSSG4lpo_v8i0HFrz8ZlA_mjHrCscU8mx2Q&oe=662ADE98',
+    },
+
+    {
+      id: 12,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t39.30808-6/348870910_641979267342226_3384115322994092371_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4Pbic4OpbL8AX_7WA3N&_nc_ht=scontent.faqp1-1.fna&oh=00_AfDj5-U6f4H09hq-0Rs9_Drnlyz7kacLwRfNeXOGYuVukw&oe=66093EA7',
+    },
+
+    {
+      id: 13,
+      img: 'https://scontent.faqp1-1.fna.fbcdn.net/v/t1.18169-9/183630_248219561969363_516409852_n.png?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=MxQtEA3XYioAX_JXl8h&_nc_ht=scontent.faqp1-1.fna&oh=00_AfAVpXXzG_fcLpTgT4dMUtmKkY_8cq_A0aH6nt6iYoZRGQ&oe=662BD1ED',
+    },
+
+  ];
