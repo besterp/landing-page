2a7616e4fb91a214c8a7e9396a001d5b78470939
besterp <110847375+besterp@users.noreply.github.com>
2024-04-16 12:10:17 -0500

    Update Historia.jsx

--- src/components/historia/Historia.jsx
+++ src/components/historia/Historia.jsx
@@ -22,7 +22,7 @@ const Historia = () => {
         </p>
        
         <p className="mt-2 text-xl text-gray-800 font-montserrat">
-          Vamos a cumplir 17 años de vida empresarial y a lo largo de dicho periodo, hemos logrado una rápida evolución, para responder a un mundo cambiante y complejo. 
+          Vamos a cumplir 18 años de vida empresarial y a lo largo de dicho periodo, hemos logrado una rápida evolución, para responder a un mundo cambiante y complejo. 
         </p>
         <p className="mt-2 text-xl text-gray-800 font-montserrat">
         A la fecha, contamos con la versión BESTERP FINANCIAL INTEGRAL 7.0 WEB. Éste es el resultado de una ardua labor desplegada por el SCRUM TEAM (equipo BESTERP) que con gran acierto ha plasmado en el Sistema Besterp, la mayor cantidad de conocimiento y mejores Practicas en Microfinanzas del mundo Cooperativo, que permite a cada uno de nuestro clientes personalizar los productos y diferenciarse en el mercado, teniendo control absoluto y visión de su negocio; así como, le permite no solo procesar operaciones de socios unipersonales; sino también, permite actuar con sociedades jurídicas (en PYMES), siendo un sistema simple e intuitivo, concebido para que tanto analistas como gestores puedan operar forma ágil.
